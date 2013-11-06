b393a14e433986ab01242ea354d39ee5f13c1988
Bryan Lam <Bryan.S.Lam@gmail.com>
2013-11-05 17:45:42 -0800

    Laid out plans and started

--- loltimer.py
+++ loltimer.py
@@ -1,1 +1,20 @@
 #Script used to keep track of important timers in-game:Buff timers, baron/dragon, enemy summoners
+
+#Things to add: Real time timer, reminders, some way to run this as a web app maybe change code to javascript later
+import sys
+print """
+Main Menu:
+1: Add New Timer
+2: List Timers
+3: Reset
+"""
+
+#Store timers in an array of tuples
+timers = []
+
+#Should be quick 
+def add(type):
+	type = type.lower()
+	if (type == "b" || type == "
+def reset:
+	timers = []
