cfad245d888a1eab0b58cad2f1ee12b2beb9badf
Bryan Lam <bryanlam@Bryans-MacBook-Pro.local>
2013-11-14 15:56:38 -0800

    working version almost finished just need to fix up small logic bugs

--- loltimer.py
+++ loltimer.py
@@ -3,15 +3,35 @@
 #Things to add: Real time timer, reminders, some way to run this as a web app maybe change code to javascript later
 import sys
 from threading import Timer
-print """
+#Store timers in an array of tuples
+timers = []
+
+def menu():
+	print """
 Main Menu:
 1: Add New Timer
 2: List Timers
 3: Reset
-"""
+4: Help
+0: Exit
+	"""
+	s = ""
+	while(s != '1' and s != '2' and s != '3' and s != '0' and s != '4'):
+		s = raw_input('>')
 
-#Store timers in an array of tuples
-timers = []
+		if(s == '1'):
+			print "Add a timer:"
+			time = raw_input('Time: ')
+			type = raw_input('Type: ')
+			add(type, int(time))
+		elif(s == '2'):
+			list()
+		elif(s == '3'):
+			reset()
+		elif(s == '0'):
+			sys.exit()
+
+
 
 #Should be quick types = b, d, eb, er, ab, ar
 #Automatically erase the entries after the elapsed time has passed
@@ -23,23 +43,23 @@ def add(type, currentTime):
 		Timer(420.0, timers.remove(x)).start()
 	#dragon
 	elif (type == "d"):
-		x = timers.insert(("Dragon", currentTime + 600))
+		x = ("Dragon", currentTime + 600)
 		Timer(360.0, timers.remove(x)).start()
 	#enemy blue
 	elif (type == "eb"):
-		x = timers.insert(("Enemy Blue", currentTime + 500))
+		x = ("Enemy Blue", currentTime + 500)
 		Timer(300.0, timers.remove(x)).start()
 	#enemy red
 	elif (type == "er"):
-		x = timers.insert(("Enemy Red", currentTime + 500))
+		x = ("Enemy Red", currentTime + 500)
 		Timer(300.0, timers.remove(x)).start()
 	#allied red
 	elif (type == "ar"):
-		x = timers.insert(("Allied Red", currentTime + 500))
+		x = ("Allied Red", currentTime + 500)
 		Timer(300.0, timers.remove(x)).start()
 	#allied blue
 	elif (type == "ab"):
-		x = timers.insert(("Allied Blue", currentTime + 500))
+		x = ("Allied Blue", currentTime + 500)
 		Timer(300.0, timers.remove(x)).start()
 
 	if (len(timers) == 0):
@@ -58,3 +78,8 @@ def list():
 
 def reset():
 	timers = []
+
+
+#Start shit up
+while(True):
+	menu()
