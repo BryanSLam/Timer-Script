60b6e68728fba3c777da3920d2f228a11fad85fb
Bryan Lam <Bryan.S.Lam@gmail.com>
2013-12-19 16:50:45 -0800

    What did I not committtt

--- timergui.py
+++ timergui.py
@@ -1,8 +1,18 @@
 #!/usr/bind/python
+
+#Have one root window and multiple frames that you place onto the root window
 from Tkinter import *
-from ttk import *
 
 root = Tk()
-root.geometry("350x300+300+300")
+root.geometry('450x400')
 root.title("Buff Timer")
+
+buttonFrame = Frame(root, width=50, height=400)
+
+addButton = Button(buttonFrame,text="Add Timer").pack(side=RIGHT)
+helpButton = Button(buttonFrame,text="Help").pack(side=RIGHT)
+resetButton = Button(buttonFrame,text = "Reset").pack(side=RIGHT)
+
+buttonFrame.pack(side=RIGHT)
+
 root.mainloop()
