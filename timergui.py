a95291d308aa60211001f6628b319969b6b17880
Bryan Lam <Bryan.S.Lam@gmail.com>
2013-12-20 15:54:31 -0800

    settling on this way of formatting, classes are too much of a bother

--- timergui.py
+++ timergui.py
@@ -2,14 +2,23 @@
 
 #Have one root window and multiple frames that you place onto the root window
 from Tkinter import *
+import tkMessageBox
 
 root = Tk()
+
+
+addTimerEntry = Entry(root).grid(row=0, column=2)
+
+def entryTest():
+	print "wert"
+
+
+addLabel = Label(root, text="Add Timer:").grid(row=0, column = 1, sticky=W)
+addButton = Button(root, text="Add",command=entryTest)
+addButton.grid(row = 0, column=3)
+resetButton = Button(root, text="Reset").grid(column = 3)
+helpButton = Button(root, text="Help").grid(column = 3)
+
 #root.geometry('450x400')
 root.title("Buff Timer")
-
-addButton = Button(root, text="Add Timer").grid(column=1)
-resetButton = Button(root, text="Reset").grid(column = 1)
-helpButton = Button(root, text="Help").grid(column = 1)
-
-
 root.mainloop()
