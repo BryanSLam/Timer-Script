628f5cc8ce6ec1c32cdb6e9db7633fc496dcc2d7
Bryan Lam <Bryan.S.Lam@gmail.com>
2013-12-20 17:25:03 -0800

    Fixed entry, last commit of day

--- timergui.py
+++ timergui.py
@@ -4,6 +4,13 @@
 from Tkinter import *
 import tkMessageBox
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 class Application(Tk):
 	def __init__(self):
 		Tk.__init__(self)
@@ -15,8 +22,16 @@ class Application(Tk):
 		self.addButton = Button(self, text="Add", command=self.testEntry)
 		self.addButton.grid(row=0, column=3)
 
+	#format = type currentTime
 	def testEntry(self):
-		print self.timerEntry.get()
+		n = self.timerEntry.get().split(' ')
+		if(n[0] != 'b' and n[0] != 'd' and n[0] != 'eb' and n[0] != 'er'
+			and n[0] != 'ar' and n[0] != 'ab'):
+			tkMessageBox.showinfo("Invalid type", "TYPE WRONG NERD")
+		elif(is_number(n[1]) == False):
+			tkMessageBox.showinfo("Invalid time", "TIME WRONG TRY AGAIN NERD")
+		else:
+			self.timerEntry.delete(0, END)
 
 app = Application()
 app.mainloop()
